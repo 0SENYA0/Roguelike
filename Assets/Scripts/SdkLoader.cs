3bcb3f69aa0a7b64a575acf73fbde6613d476700
Arsen Kuvatow <kuv99.99@yandex.ru>
2023-07-16 15:38:39 +0300

    Added Sdk loader

---
+++ Assets/Scripts/SdkLoader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using Agava.YandexGames;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class SdkLoader : MonoBehaviour
+    {
+        private void Awake()
+        {
+            StartCoroutine(LoadSdk());
+        }
+
+        private IEnumerator LoadSdk()
+        {
+#if !UNITY_WEBGL && UNITY_EDITOR
+            yield break;
+#endif
+            yield return  YandexGamesSdk.Initialize(() => SceneManager.LoadScene(1));
+        }
+    }
+}
