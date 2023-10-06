d13a63460653718748204f807f82a7c0990899e3
HomeChaos <ibi692009@yandex.ru>
2023-10-06 13:13:48 +0300

    Build for open test

--- Assets/Scripts/Player/PlayerAttackView.cs
+++ Assets/Scripts/Player/PlayerAttackView.cs
@@ -1,34 +1,17 @@
-using System.Collections.Generic;
-using System.Linq;
 using Assets.Person;
-using Assets.UI;
-using DefaultNamespace.Tools;
-using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 namespace Assets.Player
 {
     public class PlayerAttackView : UnitAttackView
     {
-        [SerializeField] private List<Image> _heart;
-        [SerializeField] private PlayerView _player;
-
         protected override void OnPastEnable()
         {
             base.OnPastEnable();
-            ChangeUIHealthValue(0);
         }
 
         public override void ChangeUIHealthValue(float value)
         {
-            int currentHeartsCount = ((int)_player.PlayerPresenter.Player.Health) / 100 + 1;
-
-            foreach (var heart in _heart)
-            {
-                heart.gameObject.SetActive(currentHeartsCount > 0);
-                currentHeartsCount--;
-            }
+            
         }
     }
 }
